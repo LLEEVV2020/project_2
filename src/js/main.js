405f3083355164439eceb2382cae6704184371e3
LLEEVV2020 <lleevv2020@gmail.com>
2022-11-02 11:20:40 +0400

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
+import './jquery.fancybox.js';
+
 import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+/**Не знаю куда добавить скипт для модалки */
+$(document).ready(function() {
+  $('.fancybox').fancybox({
+      padding : 0,
+      openEffect  : 'elastic',
+      closeBtn: false
+  });
+});
